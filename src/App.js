65af730620e3fb2e1cfca431cd2328ae08d917e0
Michael <Mlcorrieri@gmail.com>
2021-07-13 16:00:45 -0400

    about me added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AboutMe from "./Components/AboutMe";
+import Projects from "./Components/Projects";
+import MyInfo from "./Components/MyInfo";
+import NavBar from "./Components/NavBar";
+import Name from "./Components/Name";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section id="top">
+      <div className="App">
+        <NavBar />
+        <Name />
+        <AboutMe />
+        <Projects />
+        <MyInfo />
+      </div>
+    </section>
   );
 }
 
---
+++ src/Components/AboutMe.js
@@ -0,0 +1,18 @@
+function AboutMe() {
+  return (
+    <div id="aboutme">
+      <div className="aboutmebox">
+        <h2>About me</h2>
+        <p className="description">
+          My name is Michael Corrieri and I'm a software engineer based in New
+          York City. My communication skills, research experience and scientific
+          background I earned while completing my Masters of Public Health in
+          combination with newly acquired fullstack software engineering
+          education makes me a well rounded and teachable developer.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
