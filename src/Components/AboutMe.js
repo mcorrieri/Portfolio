984556011cd01ddc2ac35fe314fbdc73c7668cff
Michael <Mlcorrieri@gmail.com>
2021-11-17 17:09:24 -0500

    updated about

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -15,20 +15,18 @@ function AboutMe() {
       <div className="aboutmebox">
         <h2>About</h2>
         <p className="description">
-          Hey there! My name is Michael Corrieri and I'm a Software Developer
-          based in New York City. After moving to New York in January 2021 and
-          living with my two friends who are both Software Developers, I guess
-          that beginning my own journey into programming was inevitable.{" "}
-          <br></br>
-          <br></br> I enjoy incorporating my new found passion for programming
-          with the communication skills and research experience I gained while
-          completing my Masters of Public Health degree in New Orleans. My
-          scientific background, in combination with my newly acquired Full
-          Stack Software Engineering education, has allowed me to work on some
-          pretty cool projects and I am always ready to learn more.
+          Wie Gehts! My name is Michael Corrieri and I'm a Software Engineer
+          based in Brooklyn. I enjoy incorporating my passion for coding with
+          the communication skills and research experience I gained while
+          completing my Masters of Public Health degree.<br></br>
+          <br></br> As a developer I strive to continually incorporate new tools
+          into my repertoire, keep up with modern technologies and best
+          practices, while writing readable and reusable code. I am excited to
+          build solutions and improve software for ineffective practices within
+          healthcare and wellness industries.
         </p>
         <br></br>
-        <h2>Frameworks I Use</h2>
+        <h2>Languages/Frameworks I Use</h2>
         <div className="language-icons">
           <img
             src={JS}
@@ -44,6 +42,9 @@ function AboutMe() {
             height="65px"
             weight="65px"
           />
+          <img src={Ruby} className="icon" alt="portimg" height="65px" />
+          <img src={Rails} className="icon" alt="portimg" height="65px" />
+          <img src={PSQL} className="icon" alt="portimg" height="65px" />
           <img
             src={Mongo}
             className="icon"
@@ -52,9 +53,6 @@ function AboutMe() {
             weight="65px"
           />
           <img src={Firebase} className="icon" alt="portimg" height="65px" />
-          <img src={Ruby} className="icon" alt="portimg" height="65px" />
-          <img src={Rails} className="icon" alt="portimg" height="65px" />
-          <img src={PSQL} className="icon" alt="portimg" height="65px" />
           <img src={MUI} className="icon" alt="portimg" height="65px" />
           <img src={HTML} className="icon" alt="portimg" height="65px" />
           <img src={Semantic} className="icon" alt="portimg" height="65px" />
