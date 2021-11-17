1f643cb3ce5a47c8dcbfc9e786e5c3574ad52f0e
Michael <Mlcorrieri@gmail.com>
2021-11-17 17:18:47 -0500

    border added

--- src/Components/Name.js
+++ src/Components/Name.js
@@ -7,7 +7,7 @@ function Name() {
           Michael <br></br> Corrieri
         </h1>
         <h3 className="subname2">Full Stack</h3>
-        <h3 className="subname"> Software Developer</h3>
+        <h3 className="subname"> Software Engineer</h3>
       </div>
     </div>
   );
